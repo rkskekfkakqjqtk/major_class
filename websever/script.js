a1d7e6d4d65f5bc193eb1e21ad3675cbd1e54cac
rkskekfkakqjqtk <126747165+rkskekfkakqjqtk@users.noreply.github.com>
2023-12-13 15:55:39 +0900

    Update script.js

--- websever/script.js
+++ websever/script.js
@@ -5,17 +5,19 @@ document.addEventListener('DOMContentLoaded', function() {
         // 여기에 더 많은 게시물을 추가할 수 있습니다.
     ];
 
+    // 링크 추가 기능
     document.getElementById('addLink').addEventListener('click', function() {
-    const linkInput = document.getElementById('linkInput');
-    const link = linkInput.value;
-    if (link) {
-        // 새로운 아이템을 리스트에 추가
-        items.push({ title: `링크: ${link}`, tags: ["링크"] });
-        updateList(items);
-        linkInput.value = ''; // 입력 필드 초기화
-    }
-});
+        const linkInput = document.getElementById('linkInput');
+        const link = linkInput.value;
+        if (link) {
+            // 새로운 아이템을 리스트에 추가
+            items.push({ title: `링크: ${link}`, tags: ["링크"] });
+            updateList(items);
+            linkInput.value = ''; // 입력 필드 초기화
+        }
+    });
 
+    // 리스트 업데이트 함수
     function updateList(items) {
         const listContainer = document.getElementById('listContainer');
         listContainer.innerHTML = '';
@@ -35,6 +37,7 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     }
 
+    // 검색 필터 함수
     function filterItems(query) {
         return items.filter(item => 
             item.title.toLowerCase().includes(query.toLowerCase()) ||
@@ -42,10 +45,12 @@ document.addEventListener('DOMContentLoaded', function() {
         );
     }
 
+    // 검색 기능
     document.getElementById('searchBox').addEventListener('input', function(e) {
         const filteredItems = filterItems(e.target.value);
         updateList(filteredItems);
     });
 
-    updateList(items); // 초기 리스트 로드
+    // 초기 리스트 로드
+    updateList(items);
 });
