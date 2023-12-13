c8f08a1619fd3c011aa02d8e77a38e9fbef01fd0
rkskekfkakqjqtk <126747165+rkskekfkakqjqtk@users.noreply.github.com>
2023-12-13 15:53:13 +0900

    Update script.js

--- websever/script.js
+++ websever/script.js
@@ -5,6 +5,17 @@ document.addEventListener('DOMContentLoaded', function() {
         // 여기에 더 많은 게시물을 추가할 수 있습니다.
     ];
 
+    document.getElementById('addLink').addEventListener('click', function() {
+    const linkInput = document.getElementById('linkInput');
+    const link = linkInput.value;
+    if (link) {
+        // 새로운 아이템을 리스트에 추가
+        items.push({ title: `링크: ${link}`, tags: ["링크"] });
+        updateList(items);
+        linkInput.value = ''; // 입력 필드 초기화
+    }
+});
+
     function updateList(items) {
         const listContainer = document.getElementById('listContainer');
         listContainer.innerHTML = '';
