4504b62d294d6365b0c5ee47918475af056d80e4
rkskekfkakqjqtk <126747165+rkskekfkakqjqtk@users.noreply.github.com>
2023-12-13 15:58:40 +0900

    Update script.js

--- websever/script.js
+++ websever/script.js
@@ -1,9 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const items = [
+    let items = [
         { title: "게시물 1", tags: ["태그1", "태그2"] },
         { title: "게시물 2", tags: ["태그2", "태그3"] },
         // 여기에 더 많은 게시물을 추가할 수 있습니다.
     ];
+
+    
 
     // 링크 추가 기능
     document.getElementById('addLink').addEventListener('click', function() {
@@ -50,6 +52,43 @@ document.addEventListener('DOMContentLoaded', function() {
         const filteredItems = filterItems(e.target.value);
         updateList(filteredItems);
     });
+
+     function updateTagsList() {
+        const tagsList = document.getElementById('tagsList');
+        tagsList.innerHTML = '';
+        const allTags = new Set(items.flatMap(item => item.tags));
+
+        allTags.forEach(tag => {
+            const li = document.createElement('li');
+            li.textContent = tag;
+            li.style.cursor = 'pointer';
+            li.addEventListener('click', () => filterByTag(tag));
+            tagsList.appendChild(li);
+        });
+    }
+
+    // 태그로 필터링
+    function filterByTag(tag) {
+        const filteredItems = items.filter(item => item.tags.includes(tag));
+        updateList(filteredItems);
+    }
+
+    // 태그 추가 및 수정 기능 (예시)
+    function addOrEditTag(itemTitle, newTag) {
+        const item = items.find(item => item.title === itemTitle);
+        if (item) {
+            if (!item.tags.includes(newTag)) {
+                item.tags.push(newTag);
+            }
+            // 여기에 태그 수정 로직을 추가할 수 있습니다.
+        }
+        updateList(items);
+        updateTagsList();
+    }
+
+    // 초기 태그 목록 로드
+    updateTagsList();
+});
 
     // 초기 리스트 로드
     updateList(items);
